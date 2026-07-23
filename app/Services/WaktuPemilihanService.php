ba90d6feddda8ce06f1dfe3714b27515917611d0
Akhwan <akhwan90@gmail.com>
2026-07-23 10:44:49 +0700

    feat: add WaktuPemilihanService for handling datetime checks

---
+++ app/Services/WaktuPemilihanService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class WaktuPemilihanService
+{
+    /**
+     * Mengecek apakah waktu sekarang berada dalam rentang jadwal pemilihan
+     * berdasarkan jenis, tahun, dan npsn.
+     *
+     * @param int $jenis Jenis pemilihan
+     * @param int|string $tahun Tahun pemilihan
+     * @param string $npsn NPSN Sekolah
+     * @return array Array berisi boolean 'is_open' dan pesan status 'message'
+     */
+    public function cekJadwalBuka($jenis, $tahun, $npsn)
+    {
+        $setting = DB::table('tb_setting_waktu_pemilihan')
+            ->where('jenis', $jenis)
+            ->where('tahun', $tahun)
+            ->where('npsn', $npsn)
+            ->first();
+
+        if (!$setting || empty($setting->waktu_mulai) || empty($setting->waktu_selesai)) {
+            return [
+                'is_open' => false,
+                'message' => 'Jadwal pemilihan belum diatur.',
+                'setting' => $setting ?? null
+            ];
+        }
+
+        $now = Carbon::now();
+        $mulai = Carbon::parse($setting->waktu_mulai);
+        $selesai = Carbon::parse($setting->waktu_selesai);
+
+        // Jika waktu_mulai dan waktu_selesai bukan NULL, Carbon::parse sudah akan membaca jam/menitnya
+        if ($now->lt($mulai)) {
+            return [
+                'is_open' => false,
+                'message' => 'Waktu pemilihan belum dimulai.',
+                'setting' => $setting
+            ];
+        }
+
+        if ($now->gt($selesai)) {
+            return [
+                'is_open' => false,
+                'message' => 'Waktu pemilihan sudah berakhir.',
+                'setting' => $setting
+            ];
+        }
+
+        return [
+            'is_open' => true,
+            'message' => 'Waktu pemilihan sedang berlangsung.',
+            'setting' => $setting
+        ];
+    }
+}
