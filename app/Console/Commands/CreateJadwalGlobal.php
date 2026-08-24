1818787a6badfb824509d04647b6b6b9ef11f7fc
Nur Akhwan - T14 <akhwan90@gmail.com>
2026-08-25 04:20:30 +0700

    fix : tambahkan jam menit detik

--- app/Console/Commands/CreateJadwalGlobal.php
+++ app/Console/Commands/CreateJadwalGlobal.php
@@ -29,14 +29,14 @@ class CreateJadwalGlobal extends Command
         $listJadwals = config('pemilos.jenis_jadwal');
         $sekolahs = DB::table('tb_sekolah')->get();
         $waktu = [
-            'input_data_dps'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'pengumuman_data_dps'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'input_data_dpt'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'pengumuman_data_dpt'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'input_data_calon'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'kampanye'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'generate_token'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
-            'pemilihan'=>['mulai'=>'2026-08-01', 'selesai'=>'2026-08-31',],
+            'input_data_dps'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'pengumuman_data_dps'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'input_data_dpt'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'pengumuman_data_dpt'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'input_data_calon'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'kampanye'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'generate_token'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
+            'pemilihan'=>['mulai'=>'2026-08-01 00:01:00', 'selesai'=>'2026-10-30 23:59:00',],
         ];
 
 
