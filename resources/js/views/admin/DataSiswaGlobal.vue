5d65f281c40662db9cefe5d173bfd99cfdc41c28
Nur Akhwan - T14 <akhwan90@gmail.com>
2026-09-13 09:47:43 +0700

    fix : admin.data-siswa-global. error submit form cari

--- resources/js/views/admin/DataSiswaGlobal.vue
+++ resources/js/views/admin/DataSiswaGlobal.vue
@@ -24,7 +24,7 @@
 						<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
 							<svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" /></svg>
 						</div> -->
-						<form @submit="fetchSiswa(1)" class="flex gap-2 w-full">
+						<form @submit.prevent="fetchSiswa(1)" class="flex gap-2 w-full">
 						    <BaseInput placeholder="Query String" class="w-full" v-model="queryString"></BaseInput>
 							<BaseButton type="submit">Go</BaseButton>
 						</form>
